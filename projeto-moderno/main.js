8957ca4df2bb376f14f952ed0a086ab1fdca29b3
mateus-rodrigues21 <mateusrodrigueis4141@gmail.com>
2026-04-01 19:35:51 -0300

    atualisação do meu projetor

---
+++ projeto-moderno/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        dots: true,
+        infinity: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    })
+})
